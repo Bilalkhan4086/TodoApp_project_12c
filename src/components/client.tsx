fe14184ff41a5e60aeee80478801d617486a02ec
Muhammad Bilal <Bscs.f18.m16.a@gmail.com>
2021-04-28 11:57:34 +0500

    Update client.tsx

--- src/components/client.tsx
+++ src/components/client.tsx
@@ -3,7 +3,7 @@ import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
 export const client = new ApolloClient({
   link: new HttpLink({
-    uri: 'http://localhost:8888/.netlify/functions/g_with_f',
+    uri: '/.netlify/functions/g_with_f',
     fetch,
   }),
   cache: new InMemoryCache()

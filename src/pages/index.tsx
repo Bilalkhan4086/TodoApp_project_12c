2a15bdcb59e87d213c1f3ef0f20255bc755ed9b0
Muhammad Bilal <Bscs.f18.m16.a@gmail.com>
2021-04-28 12:05:59 +0500

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -100,7 +100,7 @@ const IndexPage:React.FC = () => {
         <form style={{ textAlign: "left" }} onSubmit={(e) => { e.preventDefault(); UpdateId.length ? updateHandler(UpdateId, UpdateValTodo, UpdateValUrl) : addHandler(UpdateValTodo, UpdateValUrl); setUpdateId(""); setUpdateValTodo(''); setUpdateValUrl(false); }}>
           <label htmlFor="todo">Todo</label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
     <input className="todo" type="text" value={UpdateValTodo} onChange={(e) => { setUpdateValTodo(e.target.value) }} /><br /><br />
-          <label htmlFor="todo">completed</label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <label htmlFor="todo">Status</label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
     <input className="done" type="checkbox" value={UpdateValUrl} checked={UpdateValUrl} onChange={() => { setUpdateValUrl(!UpdateValUrl) }} /><br /><br />
           <button disabled={UpdateValTodo === "" ? true : false}>submit</button>
         </form>
